cf2712990eb8bf9019f3431cd7f241d71bc6130f
Stanislav <stanislav@sumbera.com>
2015-06-02 23:12:50 +0200

    Initial Commit

---
+++ GeoJsonMapKitTests/GeoJsonMapKitTests.m
@@ -0,0 +1,40 @@
+//
+//  GeoJsonMapKitTests.m
+//  GeoJsonMapKitTests
+//
+//  Created by Stanislav Sumbera on 02/06/15.
+//  Copyright (c) 2015 sumbera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GeoJsonMapKitTests : XCTestCase
+
+@end
+
+@implementation GeoJsonMapKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
